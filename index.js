bfaa7521925e6e302b52b4679f3b98d6138402a7
VERMICULITE <seanowenhayes@gmail.com>
2016-03-01 22:18:51 +0000

    changed jsdom config.

--- index.js
+++ index.js
@@ -2,22 +2,24 @@
 
 let jsdom = require('jsdom');
 let through2 = require('through2');
+let fs = require('fs');
+let jquery = fs.readFileSync("./node_modules/jquery/dist/jquery.js", "utf-8");
 
 module.exports = function() {
     return through2.obj(function(page, enc, next) {
-        if(typeof next !== 'function') {
+        if (typeof next !== 'function') {
             throw new Error('next is required to be a function for mrspider-jsdom.');
         }
         var self = this;
-        jsdom.env(
-            page.content,
-            ["./node_modules/jquery/dist/jquery.js"],
-            function (err, window) {
+        jsdom.env({
+            html: page.content,
+            src: [jquery],
+            done: function(err, window) {
                 page.$ = window.$;
                 self.push(page);
                 next();
             }
-        );
+        });
 
     });
 };
