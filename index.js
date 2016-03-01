ca09bb1e705978a97a56314c5a1848fefec29d94
VERMICULITE <seanowenhayes@gmail.com>
2016-03-01 22:02:54 +0000

    added a push call

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ let jsdom = require('jsdom');
 let through2 = require('through2');
 
 module.exports = function() {
-    return through2(function(page, enc, next) {
+    return through2.obj(function(page, enc, next) {
         if(typeof next !== 'function') {
             throw new Error('next is required to be a function for mrspider-jsdom.');
         }
+        var self = this;
         jsdom.env(
             page.content,
             ["./node_modules/jquery/dist/jquery.js"],
             function (err, window) {
                 page.$ = window.$;
+                self.push(page);
                 next();
             }
         );
